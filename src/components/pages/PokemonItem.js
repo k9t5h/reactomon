7978dcef0142ec730513c4132689e6dcebcfc164
Kriszti Toth <k95toth@gmail.com>
2021-03-16 12:05:50 +0100

    Implement PokemonList component without pagination

---
+++ src/components/pages/PokemonItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ball from "../../images/poke-ball.png";
+
+export default class PokemonItem extends Component {
+  render() {
+    return (
+      <div className="card-container">
+        <div className="card">
+          <img alt="poke-ball" src={ball} height="75" />
+          <p>{this.props.name}</p>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/pages/PokemonList.js
+++ src/components/pages/PokemonList.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import axios from "axios";
+import PokemonItem from "./PokemonItem";
 
 export default class PokemonList extends Component {
+  state = {
+    next: "",
+    pokemons: [],
+  };
+
+  componentDidMount() {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((response) =>
+      this.setState({
+        next: response.data.next,
+        pokemons: response.data.results,
+      })
+    );
+  }
+
   render() {
-    return <div></div>;
+    return this.state.pokemons.map((pokemon) => (
+      <PokemonItem name={pokemon.name} url={pokemon.url} />
+    ));
   }
 }
