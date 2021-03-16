24b54a93ae9c442e3d0c8e83b0b1ac8dd54b11d2
Kriszti Toth <k95toth@gmail.com>
2021-03-16 09:12:29 +0100

    Implement Navbar component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "../layout/Navbar";
+import PokemonList from "../pages/PokemonList";
+import TypeList from "../pages/TypeList";
 
 import "./App.css";
 
 export default class App extends Component {
   render() {
-    return <div>Pokemon App</div>;
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Navbar />
+            <Route path="/pokemons" component={PokemonList} />
+            <Route path="/types" component={TypeList} />
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Link to="/pokemons">Pokemons</Link>
+        <Link to="/types">Types</Link>
+      </div>
+    );
   }
 }
