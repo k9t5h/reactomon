692c0dac9efe45664c39fed0f06b88cf5d873942
Kriszti Toth <k95toth@gmail.com>
2021-03-16 14:23:08 +0100

    Transform pokemon item to link

--- src/components/pages/pokemons/PokemonItem.js
+++ src/components/pages/pokemons/PokemonItem.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import ball from "../../../images/poke-ball.png";
 
 export default class PokemonItem extends Component {
   render() {
+    const id = this.props.url.split("/")[6];
     return (
       <div className="card-container">
         <div className="card">
           <img alt="poke-ball" src={ball} height="75" />
-          <p>{this.props.name}</p>
+          <div className="pokemon-item">
+            <Link to={`/pokemon/${id}`}>{this.props.name}</Link>
+          </div>
         </div>
       </div>
     );
