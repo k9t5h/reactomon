83d2deb5b1636661dd96254b831b1a40255bd634
Kriszti Toth <k95toth@gmail.com>
2021-03-17 15:48:41 +0100

    Rewrite the TypeList component

--- src/components/pages/types/TypeList.js
+++ src/components/pages/types/TypeList.js
@@ -1,23 +1,17 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import TypeItem from "./TypeItem";
 
-export default class TypeList extends Component {
-  state = {
-    types: [],
-  };
+export default function TypeList() {
+  const [types, setTypes] = useState([]);
 
-  componentDidMount() {
-    axios.get("https://pokeapi.co/api/v2/type").then((response) =>
-      this.setState({
-        types: response.data.results,
-      })
-    );
-  }
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/type")
+      .then((response) => setTypes(response.data.results));
+  }, []);
 
-  render() {
-    return this.state.types.map((type) => (
-      <TypeItem name={type.name} key={type.url.split("/")[6]} />
-    ));
-  }
+  return types.map((type) => (
+    <TypeItem name={type.name} key={type.url.split("/")[6]} />
+  ));
 }
