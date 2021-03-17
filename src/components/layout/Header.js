22b2d07c9332f06155fb07dc619e0031b6f2b162
Kriszti Toth <k95toth@gmail.com>
2021-03-17 14:48:58 +0100

    Rewrite the Header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,10 @@
-import { Component } from "react";
 import React from "react";
 import logo from "../../images/pokemon.png";
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div>
-        <img alt="pokemon-logo" src={logo} height="200" />
-      </div>
-    );
-  }
+export default function Header() {
+  return (
+    <div>
+      <img alt="pokemon-logo" src={logo} height="200" />
+    </div>
+  );
 }
